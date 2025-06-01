2eac9e837127ade565da0c22268b27f981632a9c
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2025-06-01 09:31:48 +0700

    Update training.py

--- training/training.py
+++ training/training.py
@@ -5,7 +5,7 @@ model = YOLO("yolov8s.pt")  # Bisa diganti ke yolov8m.pt, yolov8l.pt, dll
 
 # Mulai training
 model.train(
-    data="C:/Users/swati/Downloads/PROYEKWIRUS/datasets/data.yaml",  # path ke data.yaml
+    data="/datasets/data.yaml",  # path ke data.yaml
     epochs=10,
     imgsz=640,
     batch=16,
